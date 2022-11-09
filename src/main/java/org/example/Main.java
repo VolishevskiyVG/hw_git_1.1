152b17ff0178b8ed428ead7188b31ea095f7a4a6
Vasiliy Volishevskiy <Volishevski.V@gmail.com>
2022-11-09 21:15:13 +0300

    new prtint line

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
+
         System.out.println("Hello WoW!");
+        System.out.println("My name is Vasilii!");
+        System.out.println("I go to qa.guru");
     }
 }
